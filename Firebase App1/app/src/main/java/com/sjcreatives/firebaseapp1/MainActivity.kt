e19753cc9b162ff17fa4d389156f03044c448711
samueljuma <samueljuma1024@gmail.com>
2024-03-14 09:37:55 +0300

    write data to Firebase Database

--- Firebase App1/app/src/main/java/com/sjcreatives/firebaseapp1/MainActivity.kt
+++ Firebase App1/app/src/main/java/com/sjcreatives/firebaseapp1/MainActivity.kt
@@ -2,10 +2,37 @@ package com.sjcreatives.firebaseapp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
+import com.google.firebase.database.getValue
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+        myRef.setValue("Hello Juma")
+        val tv : TextView= findViewById(R.id.tv)
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.getValue<String>()
+                tv.text = value.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("TAGGY", "Failed to read value.", error.toException())
+            }
+
+        })
+
+
     }
 }
