213d23059ec8cfad3d178a4d7c85d124814ff3b4
samueljuma <samueljuma1024@gmail.com>
2024-03-14 09:56:47 +0300

    use recommended approach: write and read data

--- Firebase App1/app/src/main/java/com/sjcreatives/firebaseapp1/MainActivity.kt
+++ Firebase App1/app/src/main/java/com/sjcreatives/firebaseapp1/MainActivity.kt
@@ -2,36 +2,42 @@ package com.sjcreatives.firebaseapp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 import com.google.firebase.Firebase
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
-import com.google.firebase.database.getValue
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val database = Firebase.database
-        val myRef = database.getReference("message")
-        myRef.setValue("Hello Juma")
         val tv : TextView= findViewById(R.id.tv)
 
-        myRef.addValueEventListener(object : ValueEventListener {
+        //Real Time database reference
+        database = Firebase.database.reference
+
+        //write Data to Firebase
+        database.child("users").setValue("samueljuma")
+
+        //Read Data from Firebase
+        val postListener = object : ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
-                val value = snapshot.getValue<String>()
-                tv.text = value.toString()
+                val user = snapshot.value
+                tv.text = user.toString()
             }
 
             override fun onCancelled(error: DatabaseError) {
-                Log.w("TAGGY", "Failed to read value.", error.toException())
+                TODO("Not yet implemented")
             }
 
-        })
+        }
+        database.child("users").addValueEventListener(postListener)
 
 
     }
