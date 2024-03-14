b90cf9732e4d648cd8e258e66198c3d76d7c4cf9
samueljuma <samueljuma1024@gmail.com>
2024-03-14 09:09:27 +0300

    additional configuration: use Firebase BOM

--- Firebase App1/app/build.gradle.kts
+++ Firebase App1/app/build.gradle.kts
@@ -42,8 +42,15 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-analytics:21.5.1")
-    implementation("com.google.firebase:firebase-database:20.3.1")
+
+    // Import the Firebase BoM
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
+
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-database")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
