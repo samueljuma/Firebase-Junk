787bcc72fb8daff9a5c1b2341cd66a4fb2f91b1f
samueljuma <samueljuma1024@gmail.com>
2024-03-15 09:52:09 +0300

    read and write custom data to firebase

--- Firebase App1/app/src/main/java/com/sjcreatives/firebaseapp1/MainActivity.kt
+++ Firebase App1/app/src/main/java/com/sjcreatives/firebaseapp1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sjcreatives.firebaseapp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import com.google.firebase.Firebase
 import com.google.firebase.database.DataSnapshot
@@ -9,6 +10,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
+import com.google.firebase.database.getValue
 import com.sjcreatives.firebaseapp1.data.User
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +28,23 @@ class MainActivity : AppCompatActivity() {
         //Writing custom data to firebase
         val user1 = User(0,"samueljuma", "Samuel", "Juma")
         database.child("Users").child(user1.userName).setValue(user1)
+
+        //Read custom data from Firebase
+        val postListener = object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val user = snapshot.getValue<User>()
+                tv.text = user?.userName
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("TAGGY", "loadPost:onCancelled", error.toException())
+            }
+
+        }
+            database.child("Users").child(user1.userName).addValueEventListener(postListener)
+
+
+
 
 //        //write Data to Firebase
 //        database.child("users").setValue("samueljuma")
--- Firebase App1/app/src/main/java/com/sjcreatives/firebaseapp1/data/User.kt
+++ Firebase App1/app/src/main/java/com/sjcreatives/firebaseapp1/data/User.kt
@@ -1,8 +1,8 @@
 package com.sjcreatives.firebaseapp1.data
 
 data class User (
-    val id: Int,
-    val userName: String,
-    val firstName: String,
-    val lastname: String
+    val id: Int = 0,
+    val userName: String ="",
+    val firstName: String = "",
+    val lastname: String = ""
 )
